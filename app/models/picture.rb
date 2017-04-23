05d3de5d35c2fb5d8d98f74041e0b52d40431fa5
Lubmes <l.m.schukking@gmail.com>
2017-04-23 10:43:32 +0200

    Fix AWS bringing paperclip defaults to the config files

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,13 +5,13 @@ class Picture < ApplicationRecord
   has_attached_file :image,
     # :path           => ":rails_root/public/images/:id/:filename",
     # :url            => "/images/:id/:filename"
-    :storage    => :s3,
-    :bucket     => ENV["s3_bucket"],
-    :s3_region  => 'eu-west-1',
-    :s3_credentials => {
-      :access_key_id      => ENV["s3_access_key_id"],
-      :secret_access_key  => ENV["s3_secret_access_key"]
-    }
+    :storage    => :s3
+    # :bucket     => ENV["s3_bucket"],
+    # :s3_region  => 'eu-west-1',
+    # :s3_credentials => {
+    #   :access_key_id      => ENV["s3_access_key_id"],
+    #   :secret_access_key  => ENV["s3_secret_access_key"]
+    # }
 
   do_not_validate_attachment_file_type :image
 
