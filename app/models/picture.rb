71f108738bdb0c387222eccdaa85f75ba26ca3c6
Lubmes <l.m.schukking@gmail.com>
2017-04-23 10:25:31 +0200

    Fix AWS regio

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,12 +5,12 @@ class Picture < ApplicationRecord
   has_attached_file :image,
     # :path           => ":rails_root/public/images/:id/:filename",
     # :url            => "/images/:id/:filename"
-    :storage  => :s3,
-    :bucket   => ENV["s3_bucket"],
+    :storage    => :s3,
+    :bucket     => ENV["s3_bucket"],
+    :s3_region  => ENV["s3_region"],
     :s3_credentials => {
       :access_key_id      => ENV["s3_access_key_id"],
-      :secret_access_key  => ENV["s3_secret_access_key"],
-      :s3_region          => ENV["s3_region"]
+      :secret_access_key  => ENV["s3_secret_access_key"]
     }
 
   do_not_validate_attachment_file_type :image
