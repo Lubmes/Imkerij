c32f80aa798f29dff50f481913c10978d07505b6
Lubmes <l.m.schukking@gmail.com>
2017-04-24 20:37:00 +0200

    AWS yml

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,9 +5,9 @@ class Picture < ApplicationRecord
   has_attached_file :image,
     # :path           => ":rails_root/public/images/:id/:filename",
     # :url            => "/images/:id/:filename"
-    :storage    => :s3
-    # :bucket     => ENV["s3_bucket"],
-    # :s3_region  => 'eu-west-1',
+    :storage    => :s3,
+    :bucket     => ENV["AWS_BUCKET"],
+    :s3_region  => 'eu-west-1'
     # :s3_credentials => {
     #   :access_key_id      => ENV["s3_access_key_id"],
     #   :secret_access_key  => ENV["s3_secret_access_key"]
