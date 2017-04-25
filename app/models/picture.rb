5c606a508d296bed3a4bc49e4929e5ecf421170f
Lubmes <l.m.schukking@gmail.com>
2017-04-25 13:44:44 +0200

    Fix

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,7 +6,7 @@ class Picture < ApplicationRecord
     # :path           => ":rails_root/public/images/:id/:filename",
     # :url            => "/images/:id/:filename"
     :storage    => :s3,
-    # :bucket     => ENV["AWS_BUCKET"],
+    :bucket     => ENV["AWS_BUCKET"],
     :s3_region  => 'eu-west-1'
     # :s3_credentials => {
     #   :access_key_id      => ENV["s3_access_key_id"],
