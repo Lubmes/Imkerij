37146f2ed92103556ec88feb8829b76e900a5c24
Lubmes <l.m.schukking@gmail.com>
2017-04-25 20:25:18 +0200

    AWS figaro require

--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,15 @@ module ImkerijWebshop
     config.i18n.default_locale = :nl
 
     # Aws.config.update({region: 'eu-west-1'})
-    config.paperclip_defaults = {
-      :storage    => :s3,
-      :s3_region  => ENV['AWS_REGION'],
-      :s3_credentials => {
-        :bucket             => ENV['AWS_BUCKET'],
-        :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
-  end
+  #   config.paperclip_defaults = {
+  #     :storage    => :s3,
+  #     :s3_region  => ENV['AWS_REGION'],
+  #     :s3_credentials => {
+  #       :bucket             => ENV['AWS_BUCKET'],
+  #       :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+  #       :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+  #     }
+  #   }
+  # end
+  Figaro.require_keys("aws_access_key_id", "aws_secret_access_key")
 end
