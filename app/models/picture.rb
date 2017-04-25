13ba00f109c4becd64313a8c31a554b9674b93ba
Lubmes <l.m.schukking@gmail.com>
2017-04-25 19:54:44 +0200

    AWS cred

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,7 +5,11 @@ class Picture < ApplicationRecord
   has_attached_file :image,
     :storage    => :s3,
     :bucket     => Figaro.env.s3_bucket,
-    :s3_region  => 'eu-west-1'
+    :s3_region  => 'eu-west-1',
+    :s3_credentials => {
+      :access_key_id      => ENV['s3_access_key_id'],
+      :secret_access_key  => ENV['s3_secret_access_key']
+    }
 
   do_not_validate_attachment_file_type :image
 end
