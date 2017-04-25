579c15d5a0cbf7e1ee8e7df180a150652e64790b
Lubmes <l.m.schukking@gmail.com>
2017-04-25 19:12:38 +0200

    AWS rebuild

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,17 +3,8 @@ class Picture < ApplicationRecord
   belongs_to :imageable, polymorphic: true
 
   has_attached_file :image,
-    # :path           => ":rails_root/public/images/:id/:filename",
-    # :url            => "/images/:id/:filename"
     :storage    => :s3,
-    :bucket     => 'ENV["AWS_BUCKET"]',
-    :s3_region  => 'eu-west-1',
-    :s3_credentials => {
-      :access_key_id      => ENV["AWS_ACCESS_KEY"],
-      :secret_access_key  => ENV["AWS_SECRET_KEY"]
-    }
+    :bucket     => Figaro.env.s3_bucket
 
   do_not_validate_attachment_file_type :image
-
-  # validates_attachment_presence :image
 end
