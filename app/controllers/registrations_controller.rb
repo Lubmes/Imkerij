1e5450667dbf5d22fe8fbf131a85d597e30f241b
Lubmes <l.m.schukking@gmail.com>
2017-08-19 13:39:49 +0200

    Registratie redirect werkt nu ook.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,26 @@ class RegistrationsController < Devise::RegistrationsController
   include ShoppingOrder
 
   def create
-    super
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      response_to_sign_up_failure resource
+    end
+
     add_shopping_order_to_current_user
   end
 
@@ -16,6 +35,14 @@ class RegistrationsController < Devise::RegistrationsController
       [:check_out, @order]
     else
       shop_path
+    end
+  end
+
+  def response_to_sign_up_failure(resource)
+    if resource.email == "" && resource.password == nil
+      redirect_to "#{root_url}orders/#{session[:order_id]}/check_out", alert: "Uw registratie formulier was leeg."
+    elsif User.pluck(:email).include? resource.email
+      redirect_to "#{root_url}orders/#{session[:order_id]}/check_out", alert: "Opgegeven e-mailadres bestaat al."
     end
   end
 
--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,7 +1,5 @@
 class CustomFailure < Devise::FailureApp
     def redirect_url
-      #  new_user_session_url(:subdomain => 'secure')
-      # check_out_order_url
       if request.referrer == "#{root_url}u/sign_in"
         "#{root_url}u/sign_in"
       else
